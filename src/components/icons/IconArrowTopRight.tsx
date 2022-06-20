f50f088052cd57db0c80add0079b90048b24f68b
cedoor <me@cedoor.dev>
2022-06-20 18:09:49 +0200

    feat: add custom icons as react elements

--- src/components/icons/IconArrowTopRight.tsx
+++ src/components/icons/IconArrowTopRight.tsx
@@ -7,11 +7,15 @@ export type IconArrowTopRightProps = {
 
 export default function IconArrowTopRight({ width = 15, height = 15 }: IconArrowTopRightProps): JSX.Element {
     return (
-        <svg width={width} height={height} viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-                d="M5.85868 0.495117L5.85398 2.49512L10.4116 2.50578L0.808105 12.09L2.2209 13.5056L11.8507 3.89516L11.8399 8.50917L13.8399 8.51377L13.8587 0.513817L5.85868 0.495117Z"
-                fill="#758BFF"
-            />
+        <svg
+            className="custom-icon"
+            width={width}
+            height={height}
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M5.85868 0.495117L5.85398 2.49512L10.4116 2.50578L0.808105 12.09L2.2209 13.5056L11.8507 3.89516L11.8399 8.50917L13.8399 8.51377L13.8587 0.513817L5.85868 0.495117Z" />
         </svg>
     )
 }
