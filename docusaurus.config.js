94868f7e1ced48f6a6ecb535b6609dc39ce26278
cedoor <me@cedoor.dev>
2023-01-05 17:00:07 +0100

    chore: update docusaurus version to 2.2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,3 +1,10 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github")
+const darkCodeTheme = require("prism-react-renderer/themes/dracula")
+
+/** @type {import('@docusaurus/types').Config} */
 module.exports = {
     title: "Semaphore",
     tagline: "Documentation and Guides",
@@ -12,75 +19,81 @@ module.exports = {
 
     plugins: ["docusaurus-plugin-sass"],
 
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en", "es"]
+    },
+
     presets: [
         [
-            "@docusaurus/preset-classic",
-            {
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
                 docs: {
-                    path: "docs",
                     routeBasePath: "docs/",
                     sidebarPath: require.resolve("./sidebars.js"),
                     editUrl: "https://github.com/semaphore-protocol/website/edit/main/",
                     includeCurrentVersion: false
                 },
                 theme: {
-                    customCss: require.resolve("./src/css/custom.scss"),
-                    customCss2: require.resolve("./src/css/colors.scss")
+                    customCss: [require.resolve("./src/css/custom.scss")]
                 }
-            }
+            })
         ]
     ],
 
-    themeConfig: {
-        announcementBar: {
-            id: "semaphore-v3",
-            content:
-                '<b>We are pleased to announce the release of <a target="_blank" rel="noopener noreferrer" href="https://github.com/semaphore-protocol/semaphore/releases/tag/v3.0.0">Semaphore V3</a> 🎉</b>',
-            backgroundColor: "#DAE0FF",
-            textColor: "#000000"
-        },
-        prism: {
-            additionalLanguages: ["solidity"]
-        },
-        navbar: {
-            logo: {
-                alt: "Semaphore Logo",
-                src: "img/semaphore-logo.svg"
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            announcementBar: {
+                id: "semaphore-v3",
+                content:
+                    '<b>We are pleased to announce the release of <a target="_blank" rel="noopener noreferrer" href="https://github.com/semaphore-protocol/semaphore/releases/tag/v3.0.0">Semaphore V3</a> 🎉</b>',
+                backgroundColor: "#DAE0FF",
+                textColor: "#000000"
             },
-            items: [
-                {
-                    label: "Whitepaper",
-                    to: "https://semaphore.appliedzkp.org/whitepaper-v1.pdf",
-                    position: "right",
-                    className: "V1_active"
+
+            navbar: {
+                logo: {
+                    alt: "Semaphore Logo",
+                    src: "img/semaphore-logo.svg"
                 },
-                {
-                    label: "Documentation",
-                    href: "/docs/introduction",
-                    position: "right",
-                    className: "persistent"
-                },
-                {
-                    label: "Github",
-                    href: "https://github.com/semaphore-protocol",
-                    position: "right",
-                    className: "persistent"
-                }
-            ]
-        },
-        colorMode: {
-            defaultMode: "dark",
-            // Should we use the prefers-color-scheme media-query,
-            // using user system preferences, instead of the hardcoded defaultMode
-            respectPrefersColorScheme: true,
-            // Dark/light switch icon options
-            switchConfig: {
-                // Icon for the switch while in dark mode
-                darkIcon: "\u{263D}",
-                // Unicode icons such as '\u2600' will work
-                // Unicode with 5 chars require brackets: '\u{1F602}'
-                lightIcon: "\u{263C}"
+                items: [
+                    {
+                        label: "Whitepaper",
+                        to: "https://semaphore.appliedzkp.org/whitepaper-v1.pdf",
+                        position: "right",
+                        className: "V1_active"
+                    },
+                    {
+                        label: "Documentation",
+                        href: "/docs/introduction",
+                        position: "right",
+                        className: "persistent"
+                    },
+                    {
+                        label: "Github",
+                        href: "https://github.com/semaphore-protocol",
+                        position: "right",
+                        className: "persistent"
+                    },
+                    {
+                        type: "localeDropdown",
+                        position: "right",
+                        className: "persistent"
+                    }
+                ]
+            },
+            colorMode: {
+                defaultMode: "dark",
+                // Should we use the prefers-color-scheme media-query,
+                // using user system preferences, instead of the hardcoded defaultMode
+                respectPrefersColorScheme: true
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+                additionalLanguages: ["solidity"]
             }
-        }
-    }
+        })
 }
